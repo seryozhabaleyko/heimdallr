555bae8c3a8f37a99b4e45737183d38e8a83d270
seryozhabaleyko <seryozha.baleyko@gmail.com>
2020-07-18 21:26:54 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 import React from 'react';
 
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Posts from './components/Posts';
+import FetchedPosts from './components/FetchedPosts';
+import PostForm from './components/PostForm';
+
 function App() {
     return (
         <>
-            <header className="App-header">header</header>
-            <main className="App-main">main</main>
-            <footer className="App-footer">footer</footer>
+            <Header />
+            <main className="main">
+                <div className="container">
+                    <div style={{ height: '50px' }} />
+                    <PostForm />
+                    <div style={{ height: '50px' }} />
+                    <Posts
+                        posts={[
+                            { id: 1, title: 'Title here. № 1' },
+                            { id: 2, title: 'Title here. № 2' },
+                        ]}
+                    />
+                    <div style={{ height: '2px', backgroundColor: '#ccc', margin: '50px 0' }} />
+                    <FetchedPosts posts={[]} />
+                    <div style={{ height: '50px' }} />
+                </div>
+            </main>
+            <Footer />
         </>
     );
 }
