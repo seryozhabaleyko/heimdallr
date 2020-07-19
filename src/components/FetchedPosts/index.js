020db81d87b38a76532e78b54e8c158d637943c6
seryozhabaleyko <seryozha.baleyko@gmail.com>
2020-07-19 18:41:20 +0300

    Update index.js

--- src/components/FetchedPosts/index.js
+++ src/components/FetchedPosts/index.js
@@ -1,22 +1,40 @@
 import React from 'react';
+import { useSelector, shallowEqual, useDispatch } from 'react-redux';
 
 import Post from '../Post';
+import { fetchPosts } from '../../actions/posts';
+import { getFetchedPosts } from '../../selectors';
+import Spinner from '../Spinner';
+import Alert from '../Alert';
 
 import styled from './fetchedPosts.module.scss';
 
-function FetchedPosts({ posts }) {
+function FetchedPosts() {
+    const dispatch = useDispatch();
+
+    const { loading, posts, error } = useSelector((state) => getFetchedPosts(state), shallowEqual);
+
+    if (loading) {
+        return <Spinner />;
+    }
+
+    if (error) {
+        return <Alert message={`${error.name}: ${error.message}`} />;
+    }
+
     if (posts.length === 0) {
         return (
-            <button className="btn btn-primary" type="button">
+            <button
+                className="btn btn-primary"
+                type="button"
+                onClick={() => dispatch(fetchPosts())}
+            >
                 Загрузить
             </button>
         );
     }
+
     return posts.map((post) => <Post key={post.id} {...post} />);
 }
-
-FetchedPosts.defaultProps = {
-    posts: [],
-};
 
 export default FetchedPosts;
