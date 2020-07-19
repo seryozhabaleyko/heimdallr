aec1d30b2dbd5092407f3a279be404d1a0ef85f5
seryozhabaleyko <seryozha.baleyko@gmail.com>
2020-07-19 18:41:26 +0300

    Update index.js

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,14 +1,17 @@
 import React from 'react';
+import classNames from 'classnames';
 
 import Logo from '../Logo';
+import Nav from '../Nav';
 
 import styled from './navbar.module.scss';
 
 function Navbar() {
     return (
         <nav className={styled.navbar}>
-            <div className="container">
+            <div className={classNames('container', styled.grid)}>
                 <Logo />
+                <Nav />
             </div>
         </nav>
     );
