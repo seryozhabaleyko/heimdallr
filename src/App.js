27d6ab6702f03560101451447271ea058d5d5cf7
seryozhabaleyko <seryozha.baleyko@gmail.com>
2020-07-20 00:06:11 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,8 +15,10 @@ function App() {
                     <div style={{ height: '50px' }} />
                     <PostForm />
                     <div style={{ height: '50px' }} />
+                    <h2>Синхронные посты</h2>
                     <Posts />
                     <div style={{ height: '2px', backgroundColor: '#ccc', margin: '50px 0' }} />
+                    <h2>Асинхронные посты</h2>
                     <FetchedPosts posts={[]} />
                     <div style={{ height: '50px' }} />
                 </div>
