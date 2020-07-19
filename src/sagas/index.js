cd06452129fb3c974ca77f4f7ac64927f1955cee
seryozhabaleyko <seryozha.baleyko@gmail.com>
2020-07-20 00:06:13 +0300

    Update index.js

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -1,17 +1,15 @@
 import { takeEvery, put, call } from 'redux-saga/effects';
-import fetch from 'isomorphic-fetch';
 
 import { FETCH_POSTS_REQUEST } from '../constants/actionTypes';
-import { fetchPostsRequest, fetchPostsSuccess, fetchPostsFailure } from '../actions/posts';
+import { fetchPostsSuccess, fetchPostsFailure } from '../actions/posts';
 
-async function fetchPostsApi(limit) {
-    const response = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=${limit}`);
-    return await response.json();
+async function fetchPostsApi() {
+    const response = await fetch('https://jsonplaceholder.typicode.com/posts?_limit=5');
+    const json = await response.json();
+    return json;
 }
 
 function* fetchPosts(limit = 5) {
-    yield put(fetchPostsRequest());
-
     try {
         const posts = yield call(fetchPostsApi, limit);
         yield put(fetchPostsSuccess(posts));
@@ -21,5 +19,5 @@ function* fetchPosts(limit = 5) {
 }
 
 export default function* rootSaga() {
-    //yield takeEvery(FETCH_POSTS_REQUEST, fetchPosts);
+    yield takeEvery(FETCH_POSTS_REQUEST, fetchPosts);
 }
