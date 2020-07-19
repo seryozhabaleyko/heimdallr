e4dfbc3cf4828c512f058562161b5a9be8e58aa6
seryozhabaleyko <seryozha.baleyko@gmail.com>
2020-07-19 18:41:57 +0300

    Update index.js

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,16 @@
-import { createStore, compose } from 'redux';
+import { createStore, compose, applyMiddleware } from 'redux';
+import thunkMiddleware from 'redux-thunk';
+import createSagaMiddleware from 'redux-saga';
 
 import rootReducer from '../reducers';
+import { forbiddenWordsMiddleware } from '../middleware';
+
+export const sagaMiddleware = createSagaMiddleware();
 
 export default createStore(
     rootReducer,
-    compose(window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()),
+    compose(
+        applyMiddleware(thunkMiddleware, forbiddenWordsMiddleware, sagaMiddleware),
+        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+    ),
 );
